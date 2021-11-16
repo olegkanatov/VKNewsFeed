b73c9986bcae3103e6a6c7f3a5bd19faa4bd37d8
Oleg Kanatov <olegkanatov7@gmail.com>
2021-11-16 12:58:39 +0300

    Parsing Data Into Cells

--- VKNewsFeed/Newsfeed/Cell/NewsfeedCell.swift
+++ VKNewsFeed/Newsfeed/Cell/NewsfeedCell.swift
@@ -8,6 +8,18 @@
 import Foundation
 import UIKit
 
+protocol FeedCellViewModel {
+    
+    var iconUrlString: String { get }
+    var name: String { get }
+    var date: String { get }
+    var text: String? { get }
+    var likes: String? { get }
+    var comments: String? { get }
+    var shares: String? { get }
+    var views: String? { get }
+}
+
 class NewsfeedCell: UITableViewCell {
     
     static let reuseId = "NewsfeedCell"
@@ -19,9 +31,20 @@ class NewsfeedCell: UITableViewCell {
     @IBOutlet weak var likesLabel: UILabel!
     @IBOutlet weak var commentsLabel: UILabel!
     @IBOutlet weak var sharesLabel: UILabel!
-    @IBOutlet weak var veiwLabel: UILabel!
+    @IBOutlet weak var veiwsLabel: UILabel!
     
     override class func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    func set(viewModel: FeedCellViewModel) {
+        
+        nameLabel.text = viewModel.name
+        dateLAbel.text = viewModel.date
+        postLabel.text = viewModel.text
+        likesLabel.text = viewModel.likes
+        commentsLabel.text = viewModel.comments
+        sharesLabel.text = viewModel.shares
+        veiwsLabel.text = viewModel.views
     }
 }
--- VKNewsFeed/Newsfeed/NewsfeedViewController.swift
+++ VKNewsFeed/Newsfeed/NewsfeedViewController.swift
@@ -17,6 +17,8 @@ class NewsfeedViewController: UIViewController, NewsfeedDisplayLogic {
     var interactor: NewsfeedBusinessLogic?
     var router: (NSObjectProtocol & NewsfeedRoutingLogic)?
     
+    private var feedViewModel = FeedViewModel.init(cells: [])
+    
     @IBOutlet weak var table: UITableView!
     
     // MARK: Setup
@@ -45,17 +47,17 @@ class NewsfeedViewController: UIViewController, NewsfeedDisplayLogic {
         setup()
         
         table.register(UINib(nibName: "NewsfeedCell", bundle: nil), forCellReuseIdentifier: NewsfeedCell.reuseId)
+        
+        interactor?.makeRequest(request: Newsfeed.Model.Request.RequestType.getNewsfeed)
     }
     
     func displayData(viewModel: Newsfeed.Model.ViewModel.ViewModelData) {
         
         switch viewModel {
             
-        case .some:
-            print(".some ViewController")
-        case .displayNewsfeed:
-            print(".displayNewsfeed ViewController")
-            
+        case .displayNewsfeed(let feedViewModel):
+            self.feedViewModel = feedViewModel
+            table.reloadData()
         }
     }
     
@@ -65,20 +67,17 @@ extension NewsfeedViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 5
+        return feedViewModel.cells.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: NewsfeedCell.reuseId, for: indexPath) as! NewsfeedCell
         
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellViewModel = feedViewModel.cells[indexPath.row]
+        cell.set(viewModel: cellViewModel)
         
-        print("select row")
-        interactor?.makeRequest(request: .some)
+        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
